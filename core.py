2552fea7a960d6cd2c64186a04579112ebe0281f
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2018-07-24 16:15:47 -0500

    imported disk and turned inventory list into a dictionary in disk.py

--- core.py
+++ core.py
@@ -1,1 +1,11 @@
-# import disk
+import disk
+
+
+def create_inventory_dictionary(inventory_list):
+    inventory_dictionary = {}
+    for inventory in inventory_list:
+        items = inventory.split(',')
+        key = items[0].strip()
+        value = int(items[1].strip())
+        inventory_dictionary[key] = value
+    return inventory_dictionary
