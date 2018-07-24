bb627a0b118fc75202c3ab1eb3b8602f933664d8
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2018-07-24 12:33:21 -0500

    put rental_inventory in disk.py

--- disk.py
+++ disk.py
@@ -1,3 +1,8 @@
 # opening, reading, writing, appending, and saving a file
 # import core
 # import test_core
+
+
+def rental_inventory():
+    with open('inventory.txt', 'w') as file:
+        file.write()
