57ad8df439582c751d5a2dbf0b84e603d201d8c3
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2018-07-28 21:13:10 -0500

    added history(), total(), c_with_open_history(), c_with_open_inventory(), m_with_open_history(), m_with_open_inventory(), b_with_open_history(), b_with_open_inventory(), c_bring_back(), m_bring_back(), and b_bring_back() to disk.py

--- disk.py
+++ disk.py
@@ -1,9 +1,86 @@
 # opening, reading, writing, appending, and saving a file
-# import core
-# import test_core
+import core
 
 
-def rental_inventory():
-    with open('inventory.txt') as file:
-        file.readlines()
-    return file.readlines
+def history():
+    with open("history.txt") as file:
+        history = file.read()
+    print(history)
+
+
+def total():
+    total = 0
+    filename = './history.txt'
+    with open(filename) as file:
+        for line in file:
+            total += (float(line.strip()))
+    print(f'Total Revenue: ${round(total, 2)}!\n')
+
+
+def c_with_open_history():
+    inventory = core.inventory_dictionary()
+    with open("history.txt", "a") as file:
+        file.write('160.5')
+        file.write('\n')
+
+
+def c_with_open_inventory():
+    inventory = core.inventory_dictionary()
+    with open('inventory.txt', 'w') as file:
+        core.stock(inventory, '1')
+        for item in inventory.values():
+            file.write('{}, {}, {}, {}, {}'.format(
+                item['name'], item['price'], item['replacement fee'],
+                item['deposit'], item['stock']) + '\n')
+
+
+def m_with_open_history():
+    inventory = core.inventory_dictionary()
+    with open("history.txt", "a") as file:
+        file.write('8.57')
+        file.write('\n')
+
+
+def m_with_open_inventory():
+    inventory = core.inventory_dictionary()
+    with open('inventory.txt', 'w') as file:
+        stock(inventory, '2')
+        for item in inventory.values():
+            file.write('{},{},{},{},{}'.format(
+                item['name'], item['price'], item['replacement fee'],
+                item['deposit'], item['stock']) + '\n')
+
+
+def b_with_open_history():
+    inventory = core.inventory_dictionary()
+    with open("history.txt", "a") as file:
+        file.write('13.07')
+        file.write('\n')
+
+
+def b_with_open_inventory():
+    inventory = core.inventory_dictionary()
+    with open('inventory.txt', 'w') as file:
+        stock(inventory, '3')
+        for item in inventory.values():
+            file.write('{},{},{},{},{}'.format(
+                item['name'], item['price'], item['replacement fee'],
+                item['deposit'], item['stock']) + '\n')
+
+
+def c_bring_back():
+    with open('history.txt', 'a') as file:
+        file.write('-50')
+        file.write('\n')
+
+
+def m_bring_back():
+    with open('history.txt', 'a') as file:
+        file.write('-2.5')
+        file.write('\n')
+
+
+def b_bring_back():
+    with open('history.txt', 'a') as file:
+        file.write('-2')
+        file.write('\n')
